ede1d89602c548a7615458cb10751a45cc0463c5
Eleanor Byass <eleanorbyass@gmail.com>
2021-01-21 13:37:52 +0000

    ES7 For in

--- session1/index_start.js
+++ session1/index_start.js
@@ -53,16 +53,15 @@ function reverseInt(int) {
 // Return a string with the first letter of every word capitalized
 // ex. capitalizeLetters('i love javascript') === 'I Love Javascript'
 function capitalizeLetters(str) {
-	let strArr = str.toLowerCase().split(' ')
-	
+	// let strArr = str.toLowerCase().split(' ')
 	// for(let i=0; i<strArr.length; i++) {
 	// 	strArr[i] = strArr[i].substring(0,1).toUpperCase() + strArr[i].substring(1)
 	// }
 	// return strArr.join(' ')
 
-	strArr.map(item => {
-		console.log(item[0].toUpperCase())
-	})
+	// return str.toLowerCase().split(' ').map(word => word[0].toUpperCase() + word.substr(1)
+	// ).join(' ')
+
 }
 
 
@@ -70,17 +69,47 @@ function capitalizeLetters(str) {
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) {
+	let charMap = {}
+	let maxNum = 0
+	let maxChar = ''
+	str.split('').forEach(function(char){
+		if(charMap[char]){
+			charMap[char]++
+		} else {
+			charMap[char] = 1
+		}
+	})
+	for(let char in charMap){
+		if(charMap[char] > maxNum){
+			maxNum = charMap[char]
+			maxChar = char
+		}
+		return maxChar
+	}
+}
 
 
 
 // CHALLENGE 6: FIZZBUZZ
 // Write a program that prints all the numbers from 1 to 100. For multiples of 3, instead of the number, print "Fizz", for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5, print "FizzBuzz".
-function fizzBuzz() {}
+function fizzBuzz() {
+	for(let i=1; i<=100; i++) {
+		if(i % 3 === 0 && i % 5 === 0){
+			console.log('FizzBuzz')
+		} else if (i % 3 === 0) {
+			console.log('Fizz')
+		} else if (i % 5 === 0) {
+			console.log('Buzz')
+		} else {
+			console.log(i)
+		}
+	}
+}
 
 
 
 // Call Function
-const output = capitalizeLetters('i love coding');
+const output = fizzBuzz();
 
 console.log(output);
